3acacd8ad7494d8dcff52ef21f6ad70540013ed2
sibtain_usa <superali_usa@yahoo.com>
2016-02-01 20:22:16 -0500

    initial commit

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
 });
 
-server.listen(8080, function(){
+server.post("/signup", function(req, res) {
+    console.log("You Signed up!")
+    console.log(req);// "You don't have to put this it may have confuse you"
+     res.redirect("/");
+   });
+     server.listen(8080, function(){
 	console.log("Now listening on port 8080");
 });
